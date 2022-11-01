c7e09b6e60c38be9ba081058f4fcf83f3c7a51d8
Ivo Elezović <ivo@element.hr>
2022-11-01 14:14:40 +0100

    cleanup

--- ProvinceMapper/Source/Localization/Vic3StateRegionScraper.cpp
+++ ProvinceMapper/Source/Localization/Vic3StateRegionScraper.cpp
@@ -17,9 +17,6 @@ void Vic3StateRegionScraper::registerKeys()
 	registerRegex(commonItems::catchallRegex, [this](const std::string& stateName, std::istream& theStream) {
 		const auto theProvinces = Vic3StateRegionScraping(theStream).getProvinces();
 		for (const auto& province: theProvinces)
-		{
-			Log(LogLevel::Debug) << "emplacing: " << province << " - " << stateName;
 			localizations.emplace(province, stateName);
-		}
 	});
 }
