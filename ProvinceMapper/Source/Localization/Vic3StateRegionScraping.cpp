fb2c4928762952df63a8a4d397bc32b5f86b895a
Ivo Elezović <ivo@element.hr>
2023-01-07 12:13:22 +0100

    Expand vic3 locs to named provinces

--- ProvinceMapper/Source/Localization/Vic3StateRegionScraping.cpp
+++ ProvinceMapper/Source/Localization/Vic3StateRegionScraping.cpp
@@ -3,6 +3,20 @@
 #include "ParserHelpers.h"
 #include "StringUtils.h"
 
+namespace
+{
+std::string prepProvinceName(const std::string& theName)
+{
+	auto theProvince = theName;
+	std::transform(theProvince.begin(), theProvince.end(), theProvince.begin(), ::toupper);
+	if (theProvince.starts_with("X"))
+		theProvince = "x" + theProvince.substr(1, theProvince.length() - 1);
+	if (theProvince.starts_with("0X"))
+		theProvince = "x" + theProvince.substr(2, theProvince.length() - 2);
+	return theProvince;
+}
+} // namespace
+
 Vic3StateRegionScraping::Vic3StateRegionScraping(std::istream& theStream)
 {
 	registerKeys();
@@ -15,15 +29,22 @@ void Vic3StateRegionScraping::registerKeys()
 	registerKeyword("provinces", [this](std::istream& theStream) {
 		const auto theProvinces = commonItems::stringList(theStream).getStrings();
 		for (const auto& province: theProvinces)
-		{
-			auto theProvince = commonItems::remQuotes(province);
-			std::transform(theProvince.begin(), theProvince.end(), theProvince.begin(), ::toupper);
-			if (theProvince.starts_with("X"))
-				theProvince = "x" + theProvince.substr(1, theProvince.length() - 1);
-			if (theProvince.starts_with("0X"))
-				theProvince = "x" + theProvince.substr(2, theProvince.length() - 2);
-			provinces.insert(theProvince); // from "0x12345a" to x12345A
-		}
+			provinces.insert(prepProvinceName(province)); // from "0x12345a" to x12345A
+	});
+	registerKeyword("city", [this](std::istream& theStream) {
+		cities.emplace(prepProvinceName(commonItems::getString(theStream)));
+	});
+	registerKeyword("port", [this](std::istream& theStream) {
+		ports.emplace(prepProvinceName(commonItems::getString(theStream)));
+	});
+	registerKeyword("farm", [this](std::istream& theStream) {
+		farms.emplace(prepProvinceName(commonItems::getString(theStream)));
+	});
+	registerKeyword("mine", [this](std::istream& theStream) {
+		mines.emplace(prepProvinceName(commonItems::getString(theStream)));
+	});
+	registerKeyword("wood", [this](std::istream& theStream) {
+		woods.emplace(prepProvinceName(commonItems::getString(theStream)));
 	});
 	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 }
--- ProvinceMapper/Source/Localization/Vic3StateRegionScraping.h
+++ ProvinceMapper/Source/Localization/Vic3StateRegionScraping.h
@@ -11,11 +11,21 @@ class Vic3StateRegionScraping: commonItems::parser
 	Vic3StateRegionScraping(std::istream& theStream);
 
 	[[nodiscard]] const auto& getProvinces() const { return provinces; }
+	[[nodiscard]] bool isProvinceMine(const std::string& province) const { return mines.contains(province); }
+	[[nodiscard]] bool isProvinceFarm(const std::string& province) const { return farms.contains(province); }
+	[[nodiscard]] bool isProvinceWood(const std::string& province) const { return woods.contains(province); }
+	[[nodiscard]] bool isProvinceCity(const std::string& province) const { return cities.contains(province); }
+	[[nodiscard]] bool isProvincePort(const std::string& province) const { return ports.contains(province); }
 
   private:
 	void registerKeys();
 
 	std::set<std::string> provinces;
+	std::set<std::string> mines;
+	std::set<std::string> farms;
+	std::set<std::string> ports;
+	std::set<std::string> woods;
+	std::set<std::string> cities;
 };
 
 #endif // VIC3_STATE_REGION_SCRAPING
