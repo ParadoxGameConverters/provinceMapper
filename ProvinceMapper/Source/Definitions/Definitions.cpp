141faf7be9964184f8b66dbe74d1387e272f8cdc
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-05-07 17:19:43 +0200

    Remove using declarations

--- ProvinceMapper/Source/Definitions/Definitions.cpp
+++ ProvinceMapper/Source/Definitions/Definitions.cpp
@@ -7,64 +7,53 @@
 #include <ParserHelpers.h>
 namespace fs = std::filesystem;
 
-using std::string;
-using std::optional;
-using std::nullopt;
-using std::shared_ptr;
-using std::tuple;
-using std::map;
-using std::set;
-using std::ofstream;
-using std::exception;
-using std::runtime_error;
-
 namespace
 {
 
-optional<tuple<string, unsigned char, unsigned char, unsigned char, string>> parseLine(const string& line)
+std::optional<std::tuple<std::string, unsigned char, unsigned char, unsigned char, std::string>> parseLine(const std::string& line)
 {
 	try
 	{
 		auto sepLoc = line.find(';');
-		if (sepLoc == string::npos)
-			return nullopt;
+		if (sepLoc == std::string::npos)
+			return std::nullopt;
 		auto sepLocSave = sepLoc;
 		auto ID = line.substr(0, sepLoc);
 		sepLoc = line.find(';', sepLocSave + 1);
-		if (sepLoc == string::npos)
-			return nullopt;
+		if (sepLoc == std::string::npos)
+			return std::nullopt;
 		auto r = static_cast<unsigned char>(std::stoi(line.substr(sepLocSave + 1, sepLoc - sepLocSave - 1)));
 		sepLocSave = sepLoc;
 		sepLoc = line.find(';', sepLocSave + 1);
-		if (sepLoc == string::npos)
-			return nullopt;
+		if (sepLoc == std::string::npos)
+			return std::nullopt;
 		auto g = static_cast<unsigned char>(std::stoi(line.substr(sepLocSave + 1, sepLoc - sepLocSave - 1)));
 		sepLocSave = sepLoc;
 		sepLoc = line.find(';', sepLocSave + 1);
-		if (sepLoc == string::npos)
-			return nullopt;
+		if (sepLoc == std::string::npos)
+			return std::nullopt;
 		auto b = static_cast<unsigned char>(std::stoi(line.substr(sepLocSave + 1, sepLoc - sepLocSave - 1)));
 		sepLocSave = sepLoc;
 		sepLoc = line.find(';', sepLocSave + 1);
-		if (sepLoc == string::npos)
-			return nullopt;
+		if (sepLoc == std::string::npos)
+			return std::nullopt;
 		auto mapDataName = line.substr(sepLocSave + 1, sepLoc - sepLocSave - 1);
 		return std::make_tuple(ID, r, g, b, mapDataName);
 	}
-	catch (exception& e)
+	catch (std::exception& e)
 	{
 		Log(LogLevel::Warning) << "Broken Definition Line: " << line << " - " << e.what();
-		return nullopt;
+		return std::nullopt;
 	}
 }
 
 } // namespace
 
 
-void Definitions::loadDefinitions(const string& mapDataPath, const LocalizationMapper& localizationMapper, LocalizationMapper::LocType locType)
+void Definitions::loadDefinitions(const std::string& mapDataPath, const LocalizationMapper& localizationMapper, LocalizationMapper::LocType locType)
 {
 	if (!commonItems::DoesFileExist(mapDataPath + "/definition.csv"))
-		throw runtime_error("Definitions file cannot be found!");
+		throw std::runtime_error("Definitions file cannot be found!");
 
 	if (commonItems::DoesFileExist(mapDataPath + "/area.txt"))
 		eu4RegionManager.loadRegions(mapDataPath);
@@ -78,7 +67,7 @@ void Definitions::loadDefinitions(const string& mapDataPath, const LocalizationM
 
 void Definitions::parseStream(std::istream& theStream, const LocalizationMapper& localizationMapper, LocalizationMapper::LocType locType)
 {
-	string line;
+	std::string line;
 	getline(theStream, line); // discard first line.
 
 	while (!theStream.eof())
@@ -155,9 +144,9 @@ void Definitions::parseStream(std::istream& theStream, const LocalizationMapper&
 				chromaCache.insert(std::pair(pixelPack(province->r, province->g, province->b), province));
 			}
 		}
-		catch (exception& e)
+		catch (std::exception& e)
 		{
-			throw runtime_error("Line: |" + line + "| is unparseable! Breaking. (" + e.what() + ")");
+			throw std::runtime_error("Line: |" + line + "| is unparseable! Breaking. (" + e.what() + ")");
 		}
 	}
 }
@@ -178,31 +167,31 @@ void Definitions::registerBorderPixel(int x, int y, unsigned char r, unsigned ch
 		chromaItr->second->borderPixels.emplace_back(pixel);
 }
 
-optional<string> Definitions::getNameForChroma(const unsigned int chroma)
+std::optional<std::string> Definitions::getNameForChroma(const unsigned int chroma)
 {
 	if (const auto& chromaCacheItr = chromaCache.find(chroma); chromaCacheItr != chromaCache.end())
 		return chromaCacheItr->second->bespokeName();
 	else
-		return nullopt;
+		return std::nullopt;
 }
 
-optional<string> Definitions::getMiscForChroma(const unsigned int chroma)
+std::optional<std::string> Definitions::getMiscForChroma(const unsigned int chroma)
 {
 	if (const auto& chromaCacheItr = chromaCache.find(chroma); chromaCacheItr != chromaCache.end())
 		return chromaCacheItr->second->miscName();
 	else
-		return nullopt;
+		return std::nullopt;
 }
 
-optional<string> Definitions::getIDForChroma(const unsigned int chroma)
+std::optional<std::string> Definitions::getIDForChroma(const unsigned int chroma)
 {
 	if (const auto& chromaCacheItr = chromaCache.find(chroma); chromaCacheItr != chromaCache.end())
 		return chromaCacheItr->second->ID;
 	else
-		return nullopt;
+		return std::nullopt;
 }
 
-shared_ptr<Province> Definitions::getProvinceForChroma(const unsigned int chroma)
+std::shared_ptr<Province> Definitions::getProvinceForChroma(const unsigned int chroma)
 {
 	if (const auto& chromaCacheItr = chromaCache.find(chroma); chromaCacheItr != chromaCache.end())
 		return chromaCacheItr->second;
@@ -210,7 +199,7 @@ shared_ptr<Province> Definitions::getProvinceForChroma(const unsigned int chroma
 		return nullptr;
 }
 
-shared_ptr<Province> Definitions::getProvinceForID(const string& ID)
+std::shared_ptr<Province> Definitions::getProvinceForID(const std::string& ID)
 {
 	if (const auto& provinceItr = provinces.find(ID); provinceItr != provinces.end())
 		return provinceItr->second;
@@ -232,23 +221,23 @@ void Definitions::loadLocalizations(const LocalizationMapper& localizationMapper
 void Definitions::registerNeighbor(unsigned int provinceChroma, unsigned int neighborChroma)
 {
 	if (!neighborChromas.contains(provinceChroma))
-		neighborChromas.emplace(provinceChroma, set<unsigned int>{});
+		neighborChromas.emplace(provinceChroma, std::set<unsigned int>{});
 	neighborChromas.at(provinceChroma).emplace(neighborChroma);
 }
 
-map<unsigned int, set<unsigned int>> Definitions::getNeighborChromas() const
+std::map<unsigned int, std::set<unsigned int>> Definitions::getNeighborChromas() const
 {
 	return neighborChromas;
 }
 
-void Definitions::ditchAdjacencies(const string& fileName)
+void Definitions::ditchAdjacencies(const std::string& fileName)
 {
-	map<string, set<string>> adjacencies;
+	std::map<std::string, std::set<std::string>> adjacencies;
 	for (const auto& [sourceChroma, targetChromas]: neighborChromas)
 	{
 		if (const auto& sourceProvince = getIDForChroma(sourceChroma); sourceChroma)
 		{
-			adjacencies.emplace(*sourceProvince, set<string>{});
+			adjacencies.emplace(*sourceProvince, std::set<std::string>{});
 			for (const auto& targetChroma: targetChromas)
 			{
 				if (const auto& targetProvince = getIDForChroma(targetChroma); targetProvince)
@@ -256,7 +245,7 @@ void Definitions::ditchAdjacencies(const string& fileName)
 			}
 		}
 	}
-	ofstream adjacenciesFile(fileName);
+	std::ofstream adjacenciesFile(fileName);
 	for (const auto& [sourceProvince, targetProvinces]: adjacencies)
 	{
 		if (targetProvinces.empty())
@@ -269,7 +258,7 @@ void Definitions::ditchAdjacencies(const string& fileName)
 	adjacenciesFile.close();
 }
 
-void Definitions::tryToLoadProvinceTypes(const string& mapDataPath)
+void Definitions::tryToLoadProvinceTypes(const std::string& mapDataPath)
 {
 	fs::path filePath = fs::path(mapDataPath) / fs::path("default.map");
 	auto filePathStr = filePath.string();
@@ -279,7 +268,7 @@ void Definitions::tryToLoadProvinceTypes(const string& mapDataPath)
 	}
 
 	auto parser = commonItems::parser();
-	const string provinceTypesRegex = "sea_zones|wasteland|impassable_terrain|uninhabitable|river_provinces|lakes|LAKES|impassable_mountains|impassable_seas";
+	const std::string provinceTypesRegex = "sea_zones|wasteland|impassable_terrain|uninhabitable|river_provinces|lakes|LAKES|impassable_mountains|impassable_seas";
 	parser.registerRegex(provinceTypesRegex, [&](const std::string& provinceType, std::istream& stream) {
 		parser.getNextTokenWithoutMatching(stream); // equals sign
 		auto typeOfGroup = parser.getNextTokenWithoutMatching(stream).value();
@@ -290,14 +279,14 @@ void Definitions::tryToLoadProvinceTypes(const string& mapDataPath)
 			auto groupSize = provIds.size();
 			if (provIds.size() < 1 || groupSize > 2)
 			{
-				throw new runtime_error("A range of provinces should have 1 or 2 elements!");
+				throw new std::runtime_error("A range of provinces should have 1 or 2 elements!");
 			}
 
 			auto beginning = provIds[0];
 			auto end = provIds[1];
 			for (auto id = beginning; id <= end; ++id)
 			{
-				string idStr = std::to_string(id);
+				std::string idStr = std::to_string(id);
 				provinces[idStr]->provinceType = provinceType;
 			}
 		}
@@ -305,13 +294,13 @@ void Definitions::tryToLoadProvinceTypes(const string& mapDataPath)
 		{
 			for (auto id : provIds)
 			{
-				string idStr = std::to_string(id);
+				std::string idStr = std::to_string(id);
 				provinces[idStr]->provinceType = provinceType;
 			}
 		}
 		else
 		{
-			throw runtime_error("Unknown province group type: " + typeOfGroup);
+			throw std::runtime_error("Unknown province group type: " + typeOfGroup);
 		}
 	});
 	parser.IgnoreAndLogUnregisteredItems();
